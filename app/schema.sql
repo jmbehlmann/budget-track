4ce4c47f4d6197a51bca33af70bd2cbcb55e4d65
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-06-05 17:06:20 -0500

    schema init

---
+++ app/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE budget_entry (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    description TEXT NOT NULL,
+    amount REAL NOT NULL,
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
