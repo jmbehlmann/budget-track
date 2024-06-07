3b7c95295b1fe72411bb5dd53d17d9fbaa98f788
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-06-06 19:28:18 -0500

    schema

--- app/schema.sql
+++ app/schema.sql
@@ -3,6 +3,6 @@ CREATE TABLE budget_entry (
     description TEXT NOT NULL,
     amount REAL NOT NULL,
     type TEXT NOT NULL,
-    month TEXT NOT NULL, -- Format: 'YYYY-MM'
+    month TEXT NOT NULL,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
