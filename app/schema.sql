aab952f6f969a68f6d07d778c1ee8e3a00c98e76
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-06-09 10:37:57 -0500

    basic entry index template, redid schema

--- app/schema.sql
+++ app/schema.sql
@@ -1,22 +1,7 @@
-CREATE TABLE IF NOT EXISTS category (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS budget (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    month TEXT NOT NULL,
-    category_id INTEGER NOT NULL,
-    amount REAL NOT NULL,
-    FOREIGN KEY (category_id) REFERENCES category (id)
-);
-
 CREATE TABLE IF NOT EXISTS entry (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     description TEXT NOT NULL,
     amount REAL NOT NULL,
     type TEXT NOT NULL,
-    month TEXT NOT NULL,
-    category_id INTEGER,
-    FOREIGN KEY (category_id) REFERENCES category (id)
+    month TEXT NOT NULL
 );
