fce55030c704584a44c5d903e4aa4c849737ae0e
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-06-09 13:48:27 -0500

    add_entry route

--- app/routes.py
+++ app/routes.py
@@ -28,8 +28,23 @@ def entries_index():
     entries = Entry.query.all()
     return render_template('/entries/index.html', entries=entries)
 
-
+@bp.route('/entries/add', methods=['POST'])
+def add_entry():
+    if request.method == 'POST':
+        description = request.form['description']
+        amount = request.form['amount']
+        entry_type = request.form['entry_type']
+        month = request.args.get('month', get_current_month())
+        entry = Entry(description = description, amount = amount, entry_type = entry_type, month = month)
+        db.session.add(entry)
+        db.session.commit()
+        return redirect('/')
 
 # budgets routes
 
 # categories routes
+
+# description
+# amount
+# entry_type
+# month
