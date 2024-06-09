32dc9e811a58b39bea51682f0ec89a7b1e45793d
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-06-09 12:33:30 -0500

    restructured __init__.py for using sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('../instance/config.py')
 
-    from . import db
     db.init_app(app)
 
-    from . import routes
+    from . import routes, models
     app.register_blueprint(routes.bp)
-
-    @app.shell_context_processor
-    def make_shell_context():
-        return {'db': db.get_db()}
 
     return app
