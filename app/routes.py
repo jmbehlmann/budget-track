3caa8e27d8ebf28b6d42e9ae9d471d221ff21df9
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-06-09 11:27:30 -0500

    html styling

--- app/routes.py
+++ app/routes.py
@@ -32,25 +32,7 @@ def entries_index():
     entries = db.execute('SELECT * FROM entry').fetchall()
     return render_template('/entries/index.html', entries=entries)
 
-# @bp.route('/entries', methods=['POST'])
-# def create_entry():
-#     description = request.form['description']
-#     amount = float(request.form['amount'])
-#     entry_type = request.form['type']
-#     month = request.form['month']
-#     db = get_db()
-#     db.execute(
-#         'INSERT INTO entry (description, amount, type, month, category_id) VALUES (?, ?, ?, ?)',
-#         (description, amount, entry_type, month)
-#     )
-#     db.commit()
-#     return redirect(url_for('routes.index', month=month))
 
-# @bp.route('/entries/<int:id>')
-# def show_entry(id):
-#     db = get_db()
-#     entry = db.execute('SELECT id, description, amount, type, month, category_id FROM entry WHERE id = ?', (id,)).fetchone()
-#     return render_template('/entries/show.html', entry=entry)
 
 # budgets routes
 
