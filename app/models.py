0f26206cef82583f0a885b17fa749fce2536dc49
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-06-11 18:26:37 -0500

    renamed entry class to transaction, use datetime instead of date

--- app/models.py
+++ app/models.py
@@ -1,15 +1,15 @@
 from app import db
 from datetime import datetime, timezone
 
-class Entry(db.Model):
+class Transaction(db.Model):
+    __tablename__ = 'transaction'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(100), nullable=False)
     amount = db.Column(db.Float, nullable=False)
-    entry_type = db.Column(db.String(10), nullable=False)
-    month = db.Column(db.String(7), nullable=False)
-    date = db.Column(db.Date, default=datetime.now(timezone.utc))
+    transaction_type = db.Column(db.String(10), nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
-    category = db.relationship('Category', backref=db.backref('entries', lazy=True))
+    date = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
+    category = db.relationship('Category', backref=db.backref('transactions', lazy=True))
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
