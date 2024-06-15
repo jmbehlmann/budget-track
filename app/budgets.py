e028754d9ec23b992f18c8efd2ebaeb7ad2a8c90
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-06-15 16:48:24 -0500

    made index_budgets route

--- app/budgets.py
+++ app/budgets.py
@@ -1,9 +1,14 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from .models import db, Category
+from .models import db, Budget
 
 budgets_bp = Blueprint('budgets', __name__)
 
 # budgets routes
+
+@budgets_bp.route('/')
+def index_budgets():
+    budgets = Budget.query.all()
+    return render_template('budgets/index.html', budgets=budgets)
 
 @budgets_bp.route('/add', methods=['GET'])
 def add_budget():
